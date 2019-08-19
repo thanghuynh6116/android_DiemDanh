35698c541d83cd0d84cc39baf773616e3d3d9b1b
thanghuynhm2 <thanghuynhm2@gmail.com>
2019-08-19 20:17:30 +0700

    Thêm hàm common lưu thông tin đăng nhập

--- app/src/main/java/com/example/news/untils/Common.java
+++ app/src/main/java/com/example/news/untils/Common.java
@@ -1,9 +1,26 @@
 package com.example.news.untils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.blikoon.qrcodescanner.QrCodeActivity;
+import com.example.news.MainActivity;
+import com.example.news.R;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,6 +30,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Common {
+
     public static Bitmap getBitmapFromURL(String src) {
         try {
             URL url = new URL(src);
@@ -36,8 +54,9 @@ public class Common {
             return matcher.group(1);
 
         }
-        return null;
+        return "";
     }
+
 
 
     public static void saveEmail(Context context, String email)
@@ -53,4 +72,11 @@ public class Common {
         SharedPreferences preferences = context.getSharedPreferences("dangnhap", Context.MODE_PRIVATE);
         return preferences.getString("email", "");
     }
+
+   
+
+
+
+
+
 }
