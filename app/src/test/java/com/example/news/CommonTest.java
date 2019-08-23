9de97c1ef98c9efb04bf8980e7d2dc71235ea7ec
maiduchieu1997 <maiduchieu1997@gmail.com>
2019-08-24 00:52:58 +0700

    Test junit qrcode

--- app/src/test/java/com/example/news/CommonTest.java
+++ app/src/test/java/com/example/news/CommonTest.java
@@ -22,4 +22,27 @@ public class CommonTest {
         assertEquals(Common.isValidEmailId("abc@gmail.com"), true);
     }
 
+    @Test
+    public void testIsvalidQRcode1() {
+        assertEquals(Common.isValidQRCODE("abc"), false);
+    }
+
+    @Test
+    public void testIsvalidQRcode2() {
+        assertEquals(Common.isValidQRCODE("HỌ VÀ TÊN: PHAN ĐĂNG KHOA\n" +
+                "    NGÀNH: CÔNG NGHỆ THÔNG TIN\n" +
+                "    KHÓA HỌC: 2018-2022"), false);
+    }
+
+    @Test
+    public void testIsvalidQRcode3() {
+        assertEquals(Common.isValidQRCODE("HỌ VÀ TÊN: PHAN ĐĂNG KHOA\n" +
+                "    NGÀNH: CÔNG NGHỆ THÔNG TIN\n" +
+                "    KHÓA HỌC: 2018-2022\n" +
+                "    MÃ SINH VIÊN: 187IT20877\n" +
+                "    ĐỊNH DANH: 187IT20877.VLU@is-tech.vn"), true);
+    }
+
+
+
 }
